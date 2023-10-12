04dfa0dde3138f36eef287d0055d2d530cb2c530
tenrinch <rinchent95@gmail.com>
2023-10-12 13:27:43 -0400

    development mode added

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    entry: './app.js',
     plugins: [
-        new HtmlWebpackPlugin()
-    ]
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'Webpack demo app',
+            header: 'Webpack demo app',
+            metaDesc: 'Webpack description',
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    mode: 'development'
 };
